f6c8c7ec1159cdc642d05724a5f457cf415c0a4d
zoe-callaghan39 <zoe.callaghan39@gmail.com>
2022-12-06 08:26:56 +0000

    Added timer to carousel

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Button on home page
+
 const btns = document.querySelectorAll('button');
 btns.forEach((items)=>{
     items.addEventListener('click',(evt)=>{
@@ -38,9 +40,84 @@ if (alertTrigger) {
     },  3000);
 
   })
-  
-  
 }
+
+
+// Carousel timer
+
+$(function() {
+
+    
+    $t = "slide"; 
+    
+  	//variables
+    $s = 1000,  
+    $d = 5000;  
+    
+    $n = $('.slide').length; 
+    $w = $('.slide').width(); 
+  	$c = $('.container').width(); 
+   	$ss = $n * $w; // slideshow width
+  
+  	
+      function timer() {
+        $('.timer').animate({"width":$w}, $d);
+        $('.timer').animate({"width":0}, 0);
+    }
+
+        
+        $('.slide').eq($i).show();
+        
+
+        setInterval(function() {
+          timer();
+            $('.slide').eq($i).fadeOut($f);
+            if ($i == $n - 1) {
+                $i = 0;
+            } else {
+                $i++;
+            }
+            $('.slide').eq($i).fadeIn($f, function() {
+                $('.timer').css({'width' : '0'});
+            });
+
+        }, $d);
+        
+    }
+    
+    function slide() {
+      timer();
+        var setSlideCSS = {
+            'float' : 'left',
+            'display' : 'inline-block',
+          	'width' : $c
+        }
+        var setSlideShowCSS = {
+            'width' : $ss 
+        }
+        $('.slide').css(setSlideCSS);
+        $('.slideshow').css(setSlideShowCSS); 
+        
+        
+        setInterval(function() {
+            timer();
+            $('.slideshow').animate({"left": -$w}, $s, function(){
+                $('.slideshow').css('left',0).append( $('.slide:first'));
+            });
+        }, $d);
+        
+    }
+    
+    if ($t == "fade") {
+        fadeInOut();
+        
+    } if ($t == "slide") {
+        slide();
+        
+    } else {
+      
+    }
+});
 
 
 
